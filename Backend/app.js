ce0e4d47125b022fcfc5d506d09e3831f3cea134
mishra-03 <mishrapriyanshu557@gmail.com>
2021-12-05 12:38:59 +0530

    New changes made

--- Backend/app.js
+++ Backend/app.js
@@ -49,7 +49,7 @@ app.use((error, req, res, next) => {
 
 mongoose
   .connect(
-    `mongodb+srv://priyanshu:priyanshu26@cluster0.epcvc.mongodb.net/mern-prod?retryWrites=true&w=majority`,
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.epcvc.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
     { useNewUrlParser: true },
     { useUnifiedTopology: true }
   )
@@ -59,6 +59,3 @@ mongoose
   .catch(err => {
     console.log(err);
   });
-  
-  
-  
